8b2452f942053a73f6311e046ef99006b60bff5c
Brian McNitt <brian@trendmedia.com>
2014-07-28 14:53:37 -0700

    Add admin menu link and options page shell

---
+++ inc/options-page-wrapper.php
@@ -0,0 +1,52 @@
+<!-- Markup Based on: https://github.com/bueltge/WordPress-Admin-Style -->
+<div class="wrap">
+	
+	<div id="icon-options-general" class="icon32"></div>
+	<h2>Code School Badges Plugin</h2>
+	
+	<div id="poststuff">
+	
+		<div id="post-body" class="metabox-holder columns-2">
+		
+			<!-- main content -->
+			<div id="post-body-content">
+				
+				<div class="meta-box-sortables ui-sortable">
+					
+					<div class="postbox">
+					
+						<h3><span>Main Content Header</span></h3>
+						<div class="inside">
+							Content space
+						</div> <!-- .inside -->
+					
+					</div> <!-- .postbox -->
+					
+				</div> <!-- .meta-box-sortables .ui-sortable -->
+				
+			</div> <!-- post-body-content -->
+			
+			<!-- sidebar -->
+			<div id="postbox-container-1" class="postbox-container">
+				
+				<div class="meta-box-sortables">
+					
+					<div class="postbox">
+					
+						<h3><span>Sidebar Content Header</span></h3>
+						<div class="inside">
+							Content space
+						</div> <!-- .inside -->
+						
+					</div> <!-- .postbox -->
+					
+				</div> <!-- .meta-box-sortables -->
+				
+			</div> <!-- #postbox-container-1 .postbox-container -->
+			
+		</div> <!-- #post-body .metabox-holder .columns-2 -->
+		
+		<br class="clear">
+	</div> <!-- #poststuff -->
+	
+</div> <!-- .wrap -->
