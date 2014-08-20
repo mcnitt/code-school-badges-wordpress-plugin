62a3c6a240bf39460f8136cea2af6bcf7ac86bf6
Brian McNitt <brian@trendmedia.com>
2014-08-20 16:31:06 -0700

    Add Widget class

---
+++ inc/front-end.php
@@ -0,0 +1,11 @@
+<?php 		
+
+	echo $before_widget;
+
+	echo $before_title . $title . $after_title;	
+
+	echo $wpcodeschool_profile['user']['username'];
+
+	echo $after_widget; 
+
+?>
---
+++ inc/widget-fields.php
@@ -0,0 +1,4 @@
+<p>
+  <label>Title</label> 
+  <input class="widefat" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+</p>
