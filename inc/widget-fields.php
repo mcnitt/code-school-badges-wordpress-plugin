c60d6890e6ebaa15cd087ca062e8b9e09f0b2c8f
Brian McNitt <brian@trendmedia.com>
2014-08-26 17:26:12 -0700

    Widget: Set default display options

--- inc/widget-fields.php
+++ inc/widget-fields.php
@@ -1,6 +1,6 @@
 <p>
   <label>Title</label> 
-  <input class="widefat" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+  <input class="widefat" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo ($title ?: $wpcodeschool_profile['user']['username'] . '\'s Code School Badges'); ?>" />
 </p>
 
 <p>
@@ -10,7 +10,7 @@
 
 <p>
 	<label>How many of your most recent completed course badges would you you like to display?</label> 
-	<input size="4" name="<?php echo $this->get_field_name('num_badges'); ?>" type="text" value="<?php echo $num_badges; ?>" />
+	<input size="4" name="<?php echo $this->get_field_name('num_badges'); ?>" type="text" value="<?php echo ($num_badges ?: count($wpcodeschool_profile['courses']['completed'])); ?>"/>
 </p>
 
 <p>
